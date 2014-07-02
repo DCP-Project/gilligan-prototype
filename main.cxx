dd4b1eb6e1ffdcd0d22efd48677fa73dbc8fd344
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2014-07-02 05:24:28 -0500

    UTF-8 everything, like magick.  the wonders of Qt.

--- main.cxx
+++ main.cxx
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("Gilligan (Minnow Client)");
     a.setApplicationVersion("0.1-experimental");
+
+    // All DCP strings are UTF-8, per spec.
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 
     MainWindow w;
     w.show();
